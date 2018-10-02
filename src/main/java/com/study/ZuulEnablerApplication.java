bf7bcd99c04f248ef88396fe8c736e44cc30b7c5
SuprasannaBhaumik <suprasanna.bhaumik@dxc.com>
2018-10-02 18:32:48 +0530

    added custom filter
    added the custom filter. Here request header is added to all the request with the location field

--- src/main/java/com/study/ZuulEnablerApplication.java
+++ src/main/java/com/study/ZuulEnablerApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@Configuration
 public class ZuulEnablerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulEnablerApplication.class, args);
+	}
+	
+	@Bean
+	public CustomFilter customFilter() {
+		return new CustomFilter();
 	}
 }
